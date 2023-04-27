c8c6418f862880ba4d59fb803d35981617ba00ec
Ma Kangjian <raysunshine990923@163.com>
2023-04-27 16:19:41 +0800

    feat:增加List的埋点

--- lib/domain/system_channels.dart
+++ lib/domain/system_channels.dart
@@ -4,20 +4,20 @@ import 'package:flutter_buried_dot/base/top_common_function.dart';
 // systemChannels:[https://api.flutter.dev/flutter/services/SystemChannels-class.html]
 
 void setupGlobalAppLifecycleListener() {
-  // SystemChannels.lifecycle.setMessageHandler((message) async {
-  //   if (message == "AppLifecycleState.resumed") {
-  //     outputToLogcat("进入前台");
-  //   } else if (message == "AppLifecycleState.inactive") {
-  //     outputToLogcat("非活跃状态，不接受交互");
-  //   } else if (message == "AppLifecycleState.paused") {
-  //     outputToLogcat("切换到后台");
-  //   } else if (message == "AppLifecycleState.detached") {
-  //     outputToLogcat("销毁、退出状态");
-  //   } else {
-  //     outputToLogcat("!!!!!!!!!!!!!!!获取到其他状态 -> $message!!!!!!!!!!!!!!!!!");
-  //   }
-  //   return message;
-  // });
+  SystemChannels.lifecycle.setMessageHandler((message) async {
+    if (message == "AppLifecycleState.resumed") {
+      outputToLogcat("进入前台");
+    } else if (message == "AppLifecycleState.inactive") {
+      outputToLogcat("非活跃状态，不接受交互");
+    } else if (message == "AppLifecycleState.paused") {
+      outputToLogcat("切换到后台");
+    } else if (message == "AppLifecycleState.detached") {
+      outputToLogcat("销毁、退出状态");
+    } else {
+      outputToLogcat("!!!!!!!!!!!!!!!获取到其他状态 -> $message!!!!!!!!!!!!!!!!!");
+    }
+    return message;
+  });
 }
 
 void setupGlobalKeyEventListener() {
--- lib/ui/message/message_view.dart
+++ lib/ui/message/message_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_buried_dot/ui/message/message_action.dart';
 import 'package:get/get.dart';
 
@@ -22,9 +23,39 @@ class MessageWidget extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             TextButton(
+              onPressed: () {},
+              child: const Text("123"),
+            ),
+            TextButton(
               onPressed: () => onAction(MessageActionType.navigateToRootRoute),
               child: const Text("多级返回"),
             ),
+            SizedBox(
+              height: 400,
+              width: 380,
+              child: ListView.builder(
+                itemBuilder: (context, index) => GestureDetector(
+                  onTap: () {
+                    var renderObject = context.findRenderObject();
+                    var viewport = RenderAbstractViewport.of(renderObject);
+                    var listViewSize = viewport.paintBounds.size; //ListView的信息
+                    var childrenCounts =
+                        (context.widget as SliverFixedExtentList)
+                            .delegate
+                            .estimatedChildCount; // ListView的child的个数
+                    var itemExtent = (context.widget as SliverFixedExtentList)
+                        .itemExtent; // ListView的child的个数
+                  },
+                  child: Container(
+                    color: Colors.blue,
+                    height: 66,
+                    child: Text(index.toString()),
+                  ),
+                ),
+                itemCount: 100,
+                itemExtent: 66,
+              ),
+            )
           ],
         ),
       ),
